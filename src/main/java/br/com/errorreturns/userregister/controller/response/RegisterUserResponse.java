2dfa5b3022d09d1f7893449f75a9a7bebc9e86f7
Diogo Izele <diogogabrielizele@gmail.com>
2022-08-15 19:37:05 -0300

    refactor: updates response to able error returns

--- src/main/java/br/com/errorreturns/userregister/controller/response/RegisterUserResponse.java
+++ src/main/java/br/com/errorreturns/userregister/controller/response/RegisterUserResponse.java
@@ -1,12 +1,17 @@
 package br.com.errorreturns.userregister.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+
+import java.util.List;
 
 @Builder @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RegisterUserResponse {
 
     private String firstName;
     private String email;
+    private List<ErrorResponse> errors;
 }
