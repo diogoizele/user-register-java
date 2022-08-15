1892c8b879c259da0232e85eab3044c4017a043e
Diogo Izele <diogogabrielizele@gmail.com>
2022-08-15 19:35:39 -0300

    feat: adds try catch in controller

--- src/main/java/br/com/errorreturns/userregister/controller/UserController.java
+++ src/main/java/br/com/errorreturns/userregister/controller/UserController.java
@@ -2,12 +2,17 @@ package br.com.errorreturns.userregister.controller;
 
 import br.com.errorreturns.userregister.controller.request.RegisterUserRequest;
 import br.com.errorreturns.userregister.controller.response.RegisterUserResponse;
+import br.com.errorreturns.userregister.exception.ApiBaseException;
 import br.com.errorreturns.userregister.service.RegisterUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -17,7 +22,14 @@ public class UserController {
     private RegisterUserService registerUserService;
 
     @PostMapping
-    public RegisterUserResponse register(@RequestBody RegisterUserRequest request) {
-        return registerUserService.register(request);
+    public ResponseEntity<RegisterUserResponse> register(@RequestBody RegisterUserRequest request) {
+
+        try {
+            return new ResponseEntity<>(registerUserService.register(request), OK);
+        } catch (ApiBaseException e) {
+            return new ResponseEntity<>(RegisterUserResponse.builder()
+                    .errors(e.getErrors())
+                    .build(), UNPROCESSABLE_ENTITY);
+        }
     }
 }
